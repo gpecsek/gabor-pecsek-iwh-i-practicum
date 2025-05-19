89dd7fbdc3b7855715fe864fdabf6f02424f21b5
gpecsek <gabor.pecsek@gmail.com>
2025-05-19 15:30:16 +0200

    feat: homepage with CO records list, as well as css

--- index.js
+++ index.js
@@ -13,17 +13,35 @@ app.use(express.json());
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
+// Set up the headers for the API calls
+const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+}
+
+// API base URL
+const BASE_URL = 'https://api.hubapi.com/crm/v3/objects';
+
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const customObject = `${BASE_URL}/2-142915099?properties=name,publisher,price`;
+    try {
+        const resp = await axios.get(customObject, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Custom Object | HubSpot APIs', data });
+    } catch (error) {
+        console.error(error);
+    }
+})
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {})
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {})
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
